05f12ec98afdd5a7deb9277306f2b6f2ebb496ca
Aleksander Augestad <89462847+ale12012@users.noreply.github.com>
2022-06-07 01:52:15 +0200

    A python script that uses the cmd command 'attrib +/- h' to show and hide all files on your desktop

--- DesktopCleaner/main.py
+++ DesktopCleaner/main.py
@@ -1,38 +1,66 @@
 import os
-import time
 
-os.chdir(r'C:\Users\ale12012\OneDrive - vestfold og telemark fylkeskommune\skrivebord')
-folder_list = os.listdir(r'C:\Users\ale12012\OneDrive - vestfold og telemark fylkeskommune\skrivebord')
-find = open(r'C:\TopVacuum\Status.txt', "r")
-attribchoice = find.readline(1)
-print(attribchoice)
-find.close()
+programFiles = os.environ['PROGRAMFILES']
+workingPath = programFiles + "\DesktopCleaner"
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
-
-def hide():
-    with open(r'C:\TopVacuum\Status.txt', "w") as finds:
-        finds.seek(0)
-        finds.write('1')
-    with open(r'C:\TopVacuume\Mappenavn.txt', "w") as folders:
-        for item in folder_list:
-            folders.write(item + "\n")
-            os.popen('attrib +h "%s"' % item)
-            print('attrib +h %s' % item)
+# get the contents of status.txt in the current directory
+def get_status():
+    with open(f"{workingPath}\status.txt", "r") as f:
+        status = f.readline()
+        f.close()
+    return status
 
 
-def show():
-    with open(r'C:\TopVacuum\Status.txt', "w") as finds:
-        finds.seek(0)
-        finds.write('0')
-    for i in folder_list:
-        os.popen('attrib -h "%s"' % i)
+#define a function to get all files in the current directory uing os module
+def get_files():
+    files = os.listdir(desktop)
+    return files
 
 
-if attribchoice == "0":
-    hide()
+def change_status():
+    status = get_status()
+    
+    with open(f"{workingPath}\status.txt", "w") as f:
+        if status == "0":
+            f.writelines("1")
+            f.close()
+        else:
+            f.writelines("0")
+            f.close()
 
-elif attribchoice == "1":
-    show()
-else:
-    print("Not Valid... Exiting")
-    time.sleep(3)
+def get_status():
+    with open(f"{workingPath}\status.txt", "r") as f:
+        status = f.readline()
+        f.close()
+    return status
+
+def write_files():
+    files = get_files()
+    with open(workingPath + R"\\files.txt", "w") as f:
+        for file in files:
+            f.writelines(file + "\n")
+        f.close()
+
+def hide_files():
+    files = get_files()
+    for i in files:
+        os.popen(f'attrib +h "{desktop}\{i}"')
+    write_files()
+
+def show_files():
+    files = get_files()
+    for i in files:
+        os.popen(f'attrib -h "{desktop}\{i}"')
+
+
+if __name__ == "__main__":
+    status = get_status()
+    if status == "0":
+        hide_files()
+        change_status()
+    elif status == "1":
+        show_files()
+        change_status()
+    else:
+        print("error")
